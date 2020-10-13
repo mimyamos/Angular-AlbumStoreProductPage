d603460bcfe0531c3ad9963cbff6d19982c849c6
mimyamos <mimyamos04@gmail.com>
2020-10-13 16:04:07 +0100

    Completed module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -14,9 +14,8 @@ export class ProductDescriptionComponent implements OnInit {
 
   constructor(private _productService: ProductService) { }
 
-  ngOnInit(): {
-    this._productService.getAlbum(1).subscribe(response =>
-      this.albumInfo = response);
+  ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
 
 }
