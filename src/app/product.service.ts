4380c9f54bff1608ca417dca5dc8819f817178ed
mimyamos <mimyamos04@gmail.com>
2020-10-13 21:15:00 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -11,7 +11,7 @@ constructor(private _http: Http) { }
   
   getAlbum(id: number) {
 
-    return this._http.get(this._albumUrl) .map((response: any) => Response.json());
+    return this._http.get(this._albumUrl) .map((response) => Response.json());
   }
 
 }
