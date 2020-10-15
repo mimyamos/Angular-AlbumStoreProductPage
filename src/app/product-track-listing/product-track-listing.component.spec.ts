756cb1a35c1ff7220526126e9e35ce3687f89fe8
mimyamos <mimyamos04@gmail.com>
2020-10-15 15:04:56 +0100

    Completed module

---
+++ src/app/product-track-listing/product-track-listing.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProductTrackListingComponent } from './product-track-listing.component';
+
+describe('ProductTrackListingComponent', () => {
+  let component: ProductTrackListingComponent;
+  let fixture: ComponentFixture<ProductTrackListingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProductTrackListingComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductTrackListingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/product-track-listing/product-track-listing.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ProductService } from './product.service';
+
+import { Album } from '../album';
+
+@Component({
+  selector: 'app-product-track-listing',
+  templateUrl: './product-track-listing.component.html',
+  styleUrls: ['./product-track-listing.component.css']
+})
+export class ProductTrackListingComponent implements OnInit {
+
+  albumInfo: Album;
+
+  constructor(private _productService: ProductService) {}
+
+  ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => 
+    this.albumInfo = response);
+  }
+
+}
